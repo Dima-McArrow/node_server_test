20254d50fb4a6000b9b1a2bc57c91ff50ce31126
Dima-McArrow <dsm.box@gmail.com>
2024-04-18 20:04:35 +0200

    changes to server.js

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ const port = process.env.PORT || 3000;
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 // Connect to the database
